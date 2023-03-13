82af987666b6c1be116fbf0f3e2d35b25c490c94
MakotoAoki <aokimakoto0322@gmail.com>
2023-03-13 21:34:49 +0900

    v-forについてのサンプル追加

--- src/components/ControlSample.vue
+++ src/components/ControlSample.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const menuList = ref(["すいか", "いちご", "メロン", "バナナ"])
 
@@ -10,14 +10,39 @@ const cheeseList = ref({
     48: "マスカルポーネ"
 })
 
-const coffeeList = [
+
+// v-forでリスト表示するが、5000円の豆しか表示しない（filterして表示する）
+const coffeeList = ref([
     {id: 1, name: "ブルーマウンテン", price: 5000},
     {id: 2, name: "キリマンジャロ", price: 5200},
     {id: 3, name: "コナ", price: 4800},
-]
+])
 
+const coffeeCalc = computed(
+    () => {
+        const newList = coffeeList.value.filter(
+            (element) => {
+                return element.price == 5000
+            }
+        )
+        return newList
+    }
+)
 
+// Map型の操作
+const pcListInit = new Map<number, string>()
+const pcList = ref(pcListInit)
 
+const changePcList = () => {
+    pcList.value.clear()
+    pcList.value.set(1, "Lenovo")
+    pcList.value.set(2, "NEC")
+    pcList.value.set(3, "Panasonic")
+}
+
+const addPcList = () => {
+    pcList.value.set(4, "Microsoft")
+}
 
 </script>
 
@@ -30,6 +55,20 @@ const coffeeList = [
         <ul>
             <li v-for="(cheese, key, index) in cheeseList" v-bind:key="cheese">チーズ名：{{ cheese }}　ID：{{ key }}　index: {{ index }}</li>
         </ul>
+
+        <ul>
+            <li v-for="r in 5" v-bind:key="r">半径{{ r }}の円の円周: {{ 2 * r * 3.14 }}</li>
+        </ul>
+
+        <ul>
+            <li v-for="coffeeitem in coffeeCalc" v-bind:key="coffeeitem.id">{{ coffeeitem.name }}</li>
+        </ul>
+
+        <ul>
+            <li v-for="[id, item] in pcList" v-bind:key="id">{{ item }}</li>
+        </ul>
+        <button v-on:click="changePcList">PCリスト</button>
+        <button v-on:click="addPcList">PC追加</button>
     </div>
     
 </template>
